7f04b69ac6fa6831e38e1835d08ed53812da475d
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-19 20:54:05 -0400

    Chat works now

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ io.on('connection', function(socket){
   });
 });
 
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		console.log('message: ' + msg);
+	});
+});
+
+io.on('connection', function(socket) {
+	socket.on('chat message', function(msg) {
+		io.emit('chat message', msg);
+	});
+});
+
 http.listen(3000, function() {
 	console.log('listening on *:3000');
 });
