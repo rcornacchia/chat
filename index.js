150508a7aff7df67fb46e9d5d2cc35c854195cbd
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-20 23:16:14 -0400

    tidying up index.js

--- index.js
+++ index.js
@@ -23,13 +23,6 @@ io.on('connection', function(socket) {
 	});
 });
 
-// // emit incoming messages (emit sends event to everyone)
-// io.on('connection', function(socket) {
-// 	socket.on('chat message', function(msg) {
-// 		io.emit('chat message', msg);
-// 	});
-// });
-
 http.listen(3000, function() {
 	console.log('listening on *:3000');
 });
