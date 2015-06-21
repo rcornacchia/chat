a7c2abcc7c2aa4dd8a14c00caed57b34a5130c5d
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-06-20 22:20:52 -0400

    removed ability to send null messages

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// routing for / --> index.html
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+// log when a user connects or disconnects
 io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('disconnect', function(){
@@ -13,17 +15,20 @@ io.on('connection', function(socket){
   });
 });
 
+// log incomming messages to the console
 io.on('connection', function(socket) {
 	socket.on('chat message', function(msg) {
 		console.log('message: ' + msg);
+		if(msg){io.emit('chat message', msg);}
 	});
 });
 
-io.on('connection', function(socket) {
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
-	});
-});
+// // emit incoming messages (emit sends event to everyone)
+// io.on('connection', function(socket) {
+// 	socket.on('chat message', function(msg) {
+// 		io.emit('chat message', msg);
+// 	});
+// });
 
 http.listen(3000, function() {
 	console.log('listening on *:3000');
